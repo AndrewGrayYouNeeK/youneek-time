fb56074e845744dc7b9ef7d9a0c84a6b429b3225
Andrew Gray <youneekartifacts@gmail.com>
2026-03-27 09:14:54 +0000

    File changes

--- src/components/younEEK/ClockTicks.jsx
+++ src/components/younEEK/ClockTicks.jsx
@@ -18,8 +18,8 @@ const OUTER_TICKS = Array.from({ length: 24 }, (_, index) => {
 const INNER_TICKS = Array.from({ length: 100 }, (_, index) => {
   const angle = (index / 100) * Math.PI * 2 - Math.PI / 2;
   const isMajor = index % 10 === 0;
-  const innerRadius = 168;
-  const outerRadius = isMajor ? 173 : 173;
+  const innerRadius = 145;
+  const outerRadius = isMajor ? 155 : 155;
 
   return {
     x1: 200 + Math.cos(angle) * innerRadius,
