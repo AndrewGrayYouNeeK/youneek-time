57951e8e547ef07493812c508e067cbee8f52ac3
Andrew Gray <youneekartifacts@gmail.com>
2026-03-27 09:43:20 +0000

    File changes

--- src/components/younEEK/ClockHands.jsx
+++ src/components/younEEK/ClockHands.jsx
@@ -6,11 +6,11 @@ export default function ClockHands({ unitRotation, minuteRotation, secondRotatio
       </div>
 
       <div className="absolute inset-0" style={{ transform: `rotate(${unitRotation}deg)` }}>
-        <div className="absolute left-1/2 top-[1%] h-[30%] w-[2.5px] -translate-x-1/2 rounded-full bg-gradient-to-b from-[#00ff88] via-[#00ff88] to-transparent" />
+        <div className="absolute left-1/2 top-[1%] h-[30%] w-[2px] -translate-x-1/2 rounded-full bg-gradient-to-b from-[#00ff88] via-[#00ff88] to-transparent" />
       </div>
 
       <div className="absolute inset-0 opacity-90" style={{ transform: `rotate(${secondRotation}deg)` }}>
-        <div className="absolute left-1/2 top-[27%] h-[23%] w-[1.5px] -translate-x-1/2 rounded-full bg-gradient-to-b from-[#00ff88] via-[#00ff88] to-transparent" />
+        <div className="absolute left-1/2 top-[27%] h-[23%] w-[2px] -translate-x-1/2 rounded-full bg-gradient-to-b from-[#00ff88] via-[#00ff88] to-transparent" />
       </div>
 
       <div className="absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rounded-full border border-[#00ff88] bg-[#080808] animate-pulse" />
